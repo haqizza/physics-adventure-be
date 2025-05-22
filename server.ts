dd69374205544c46eef7bc87b90d8685a0bef948
haqizza <haqizza@gmail.com>
2025-05-22 11:19:42 +0700

    fix: :bug: Add HOST to env. vars

--- server.ts
+++ server.ts
@@ -4,9 +4,10 @@ import Server from "./src/index";
 const app: Application = express();
 const server: Server = new Server(app);
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
+const HOST: string = process.env.HOST || 'localhost';
 
 app
-  .listen(PORT, "localhost", function () {
+  .listen(PORT, HOST, function () {
     console.log(`Server is running on port ${PORT}.`);
   })
   .on("error", (err: any) => {
