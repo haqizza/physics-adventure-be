1616525a9b28dfd84b8667fc68e91eb6a5dd4ef7
haqizza <haqizza@gmail.com>
2025-05-25 09:59:45 +0700

    feat: :goal_net: Added logging & auto-restart when exception occur

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 import cors, { CorsOptions } from "cors";
 import Routes from "./routes";
+import { logger } from './middleware/errorLogging';
 
 const FE_HOST: string = process.env.FE_HOST || 'localhost';
 
@@ -18,5 +19,16 @@ export default class Server {
     app.use(cors(corsOptions));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
+
+    app.use((err: any, req: any, res: any, next: any) => {
+      logger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+      res.status(err.status || 500).send('Something broke!');
+    });
+
+    process.on('uncaughtException', function(event) {
+      console.log('An error has occured. error is: %s and stack trace is: %s', event, event.stack);
+      console.log("Process will restart now.");
+      process.exit(1);
+    })
   }
 }
