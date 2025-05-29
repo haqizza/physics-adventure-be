0054a9123f1aa3c154ab5d192c8e30c4a79e5ec7
haqizza <haqizza@gmail.com>
2025-05-30 06:51:10 +0700

    fix: :bug: Fix mysql connection error
    Change mysql2 connection from create connection to connection pool

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,19 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-});
-
-db.connect(err => {
-  if (err) {
-    console.error('Database connection failed:', err.stack);
-    return;
-  }
-  console.log('Connected to MySQL database.');
-});
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+})
 
 export default db;
